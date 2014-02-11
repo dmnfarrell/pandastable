a4eadbfffa136c897b8b524a4d8424db827f9245
farrell.damien@gmail.com <farrell.damien@gmail.com@c7c09e87-b3be-f776-313c-63292bcc8aec>
2014-02-11 14:59:33 +0000

    first commit, pandastable source

---
+++ data.py
@@ -0,0 +1,266 @@
+#!/usr/bin/env python
+"""
+    Module implementing the Data class that manages data for
+    it's associated PandasTable.
+
+    Created Jan 2014
+    Copyright (C) Damien Farrell
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+"""
+
+from types import *
+import operator
+import string, types, copy
+import pickle
+import numpy as np
+import pandas as pd
+
+class TableModel(object):
+    """A data model for the Table class that uses pandas"""
+
+    keywords = {'colors':'colors'}
+
+    def __init__(self, dataframe=None, rows=50, columns=10):
+        """Constructor"""
+        self.initialiseFields()
+        self.setup(dataframe, rows, columns)
+        return
+
+    def setup(self, dataframe, rows=50, columns=10):
+        """Create table model"""
+        if not dataframe is None:
+            self.df = dataframe           
+        else:            
+            #colnames = list(string.ascii_lowercase[:columns])
+            #self.df = pd.DataFrame(index=range(rows),columns=colnames)
+            self.df = self.getSampleData()
+    
+        self.reclist = self.df.index # not needed now?
+        return
+
+    def getSampleData(self):
+        """Generate sample data"""
+        colnames = list(string.ascii_lowercase[:10])
+        df = pd.DataFrame(np.random.randn(200, 10), columns=colnames)
+        df = np.round(df, 3)
+        cats = ['foo','bar','sel']
+        df['label'] = [cats[i] for i in np.random.randint(0,3,200)]
+        return df
+
+    def initialiseFields(self):
+        """Create meta data fields"""
+        self.meta = {}
+        self.columnwidths = {} #used to store col widths
+        return
+
+    def save(self, filename):
+        self.df.to_msgpack(filename)
+        return
+
+    def load(self, filename):
+        self.df = pd.read_msgpack(filename)
+        return
+
+    def getlongestEntry(self, columnIndex):
+        """Get the longest cell entry in the col"""
+        '''collist = self.getColCells(columnIndex)
+        maxw=5
+        for c in collist:
+            try:
+                w = len(str(c))
+            except UnicodeEncodeError:
+                pass
+            if w > maxw:
+                maxw = w'''
+        #print 'longest width', maxw
+        maxw=5
+        return maxw
+
+    def getRecordAtRow(self, rowIndex):
+        """Get the entire record at the specifed row."""
+        name = self.getRecName(rowIndex)
+        record = self.df.ix[name]
+        return record
+
+    def getRecordIndex(self, recname):
+        
+        return rowIndex
+
+    def moveColumn(self, oldindex, newindex):
+        """Changes the order of columns"""
+
+        df = self.df
+        cols = list(df.columns)
+        name = cols[oldindex]
+        del cols[oldindex]
+        cols.insert(newindex, name)
+        self.df = df[cols]
+        print(df.columns)
+        return
+
+    def addRow(self, key=None, **kwargs):
+        """Add a row"""
+
+        return key
+
+    def deleteRow(self, rowindex=None, update=True):
+        """Delete a row"""
+        df = self.df
+        df.drop(df.index[rowindex],inplace=True)
+        return
+
+    def deleteRows(self, rowlist=None):
+        """Delete multiple or all rows"""
+        df = self.df
+        df.drop(df.index[rowlist],inplace=True)
+        return
+
+    def addColumn(self, colname=None, coltype=None):
+        """Add a column"""
+
+        return
+
+    def deleteColumn(self, colindex):
+        """delete a column"""
+        df = self.df
+        colname = df.columns[colindex]
+        del df[colname]
+        return
+
+    def deleteColumns(self, cols=None):
+        """Remove all cols or list provided"""
+        for c in cols:
+            self.deleteColumn(c)
+        return
+
+    def groupby(self, cols):
+        df = self.df
+        colnames = df.columns[cols]
+        grps = df.groupby(colnames)
+        return grps
+
+    def autoAddRows(self, numrows=None):
+        """Automatically add x number of records"""
+
+        return keys
+
+    def autoAddColumns(self, numcols=None):
+        """Automatically add x number of cols"""
+
+        return
+
+    def getColumnType(self, columnIndex):
+        """Get the column type"""
+        coltype = self.df.dtypes[columnIndex]
+        return coltype
+
+    def getColumnCount(self):
+         """Returns the number of columns in the data model"""
+         return len(self.df.columns)
+
+    def getColumnName(self, columnIndex):
+         """Returns the name of the given column by columnIndex"""
+         return self.df.columns[columnIndex]
+
+    def getColumnData(self, columnIndex=None, columnName=None,
+                        filters=None):
+        """Return the data in a list for this col,
+            filters is a tuple of the form (key,value,operator,bool)"""
+        if columnIndex != None and columnIndex < len(self.columnNames):
+            columnName = self.getColumnName(columnIndex)
+        names = Filtering.doFiltering(searchfunc=self.filterBy,
+                                         filters=filters)
+        coldata = [self.data[n][columnName] for n in names]
+        return coldata
+
+    def getColumns(self, colnames, filters=None, allowempty=True):
+        """Get column data for multiple cols, with given filter options,
+            filterby: list of tuples of the form (key,value,operator,bool)
+            allowempty: boolean if false means rows with empty vals for any
+            required fields are not returned
+            returns: lists of column data"""
+
+        def evaluate(l):
+            for i in l:
+                if i == '' or i == None:
+                    return False
+            return True
+        coldata=[]
+        for c in colnames:
+            vals = self.getColumnData(columnName=c, filters=filters)
+            coldata.append(vals)
+        if allowempty == False:
+            result = [i for i in zip(*coldata) if evaluate(i) == True]
+            coldata = list(zip(*result))
+        return coldata
+
+    def getRowCount(self):
+         """Returns the number of rows in the table model."""
+         return len(self.reclist)
+
+    def getValueAt(self, rowindex, colindex):
+         """Returns the cell value at location specified
+             by columnIndex and rowIndex."""
+
+         df = self.df
+         value = self.df.iloc[rowindex,colindex]
+         if type(value) is float and np.isnan(value):
+             return ''
+         return value
+
+    def setValueAt(self, value, rowIndex, columnIndex):
+        """Changed the dictionary when cell is updated by user"""
+        name = self.getRecName(rowIndex)
+        colname = self.getColumnName(columnIndex)
+        coltype = self.columntypes[colname]
+        if coltype == 'number':
+            try:
+                if value == '': #need this to allow deletion of values
+                    self.data[name][colname] = ''
+                else:
+                    self.data[name][colname] = float(value)
+            except:
+                pass
+        else:
+            self.data[name][colname] = value
+        return
+
+    def getRecColNames(self, rowIndex, ColIndex):
+        """Returns the rec and col name as a tuple"""
+        recname = self.getRecName(rowIndex)
+        colname = self.getColumnName(ColIndex)
+        return (recname, colname)
+
+    def getRecAtRow(self, recname, colname, offset=1, dim='y'):
+        """Get the record name at a specified offset in the current
+           table from the record given, by using the current sort order"""
+        thisrow = self.getRecordIndex(recname)
+        thiscol = self.getColumnIndex(colname)
+        #table goto next row
+        if dim == 'y':
+            nrow = thisrow + offset
+            ncol = thiscol
+        else:
+            nrow = thisrow
+            ncol = thiscol + offset
+
+        newrecname, newcolname = self.getRecColNames(nrow, ncol)
+        print('recname, colname', recname, colname)
+        print('thisrow, col', thisrow, thiscol)
+        return newrecname, newcolname
+
+    def __repr__(self):
+        return 'Table Model with %s rows' %len(self.df)
---
+++ plotting.py
@@ -0,0 +1,286 @@
+#!/usr/bin/env python
+"""
+    Module for pylab plotting helper classes.
+
+    Created Jan 2014
+    Copyright (C) Damien Farrell
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+"""
+
+import types
+from tkinter import *
+from tkinter.ttk import *
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+from matplotlib.lines import Line2D
+#import matplotlib.animation as animation
+import tkinter.font
+
+class PlotFrame(Frame):
+    """Provides a frame for figure canvas and MPL settings"""
+
+    def __init__(self,parent=None):
+
+        self.parent=parent         
+        if self.parent != None:
+            Frame.__init__(self, parent)
+            self.main = self.master
+        else:
+            self.main = Toplevel()
+            self.master = self.main
+            self.main.title('Plot Viewer')
+        self.addFigure(self.main)
+        self.nb = Notebook(self.main)
+        self.nb.pack(side=TOP,fill=BOTH)
+        self.mplopts = MPLoptions()
+        w = self.mplopts.showDialog(self.nb, callback=self.redraw)
+        self.nb.add(w, text='options')        
+        #self.anim = animator(self.nb, plotframe=self)
+        #self.nb.add(self.anim, text='animation')
+        return
+
+    def addFigure(self, parent):
+        from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
+        from matplotlib.figure import Figure
+        self.fig = f = Figure(figsize=(5,4), dpi=100)
+        a = f.add_subplot(111)
+        canvas = FigureCanvasTkAgg(f, master=parent)
+        canvas.show()
+        canvas.get_tk_widget().pack(side=TOP, fill=BOTH, expand=1)
+        toolbar = NavigationToolbar2TkAgg(canvas, parent)
+        toolbar.update()
+        canvas._tkcanvas.pack(side=TOP, fill=BOTH, expand=1)
+        self.ax = a
+        self.canvas = canvas
+        return
+
+    def redraw(self):
+        """Redraw the current axis"""
+
+        if not hasattr(self, 'data'):
+            return
+        data = self.data
+        kwds = self.mplopts.kwds
+        ignore = ['font','fontsize']
+        for i in ignore:
+            if i in kwds:
+                del kwds[i]
+        if len(data.columns)==1:
+            kwds['subplots']=0
+        kind = kwds['kind']
+        if kind == 'scatter':
+            kwds['x'] = data.columns[0]
+            kwds['y'] = data.columns[1]
+            if kwds['marker'] == '':
+                kwds['marker'] = 'o'
+        elif kind == 'bar' or kind == 'barh':
+            if 'marker' in kwds:
+                del kwds['marker']
+            if 'linestyle' in kwds:
+                del kwds['linestyle']
+
+        if kind != 'scatter':
+            if 's' in kwds:
+                del kwds['s']
+       
+        self.ax.clear()
+        ax = self.ax
+        if kind == 'boxplot':         
+            data.boxplot(ax=ax)
+        elif kind == 'histogram': 
+            data.hist(ax=ax) 
+        elif kind == 'heatmap':
+            self.fig.clear()
+            self.ax = ax = self.fig.add_subplot(111)
+            hm = ax.pcolor(data, cmap=kwds['colormap'])
+            self.fig.colorbar(hm, ax=ax)
+            #ax.set_yticks(np.arange(0.5, len(data.index), 1), data.index)
+            #ax.set_xticks(np.arange(0.5, len(data.columns), 1), data.columns)
+            #ax.set_aspect('equal') 
+        else:
+            data.plot(ax=ax, **kwds)
+        self.canvas.draw()
+        return 
+
+class animator(Frame):
+    
+    def __init__(self, parent, plotframe):
+        Frame.__init__(self, parent)
+        self.main = Frame(self)
+        self.main.pack(side=TOP, fill=BOTH, expand=1)
+        self.plotframe = plotframe
+        self.doGUI()
+        return
+
+    def doGUI(self):            
+        b = Button(self.main, text='animate', command=self.animate)
+        b.pack(side=TOP)
+        l = Label(self.main, text='arse')
+        l.pack(side=TOP)
+        return
+
+    def animate(self):
+
+        def run(i):
+            line.set_ydata(np.sin(x+i/10.0))
+            return line,
+        fig = self.plotframe.fig
+        data = self.plotframe.data
+        ax = self.plotframe.ax
+        ax.clear()
+        x = np.arange(0, 2*np.pi, 0.01)   
+        line, = ax.plot(x, np.sin(x))
+        ani = animation.FuncAnimation(fig, run, np.arange(1, 200),
+                            blit=True, interval=10,
+                            repeat=False)
+        return
+
+    def run(data, ax):
+        """update the data"""
+        t,y = data
+        xdata.append(t)
+        ydata.append(y)
+        xmin, xmax = ax.get_xlim()
+        if t >= xmax:
+            ax.set_xlim(xmin, 2*xmax)
+            ax.figure.canvas.draw()
+        line.set_data(xdata, ydata)
+        return line,
+
+class MPLoptions(object):
+    """Class to provide a gui for matplotlib options handling"""
+
+    colormaps = sorted(m for m in plt.cm.datad if not m.endswith("_r"))
+    markers = ['','o','.','^','v','>','<','s','+','x','p','d','h','*']
+    linestyles = ['-','--','-.',':','steps']
+    legend_positions = ['best', 'upper left','upper center','upper right',
+                         'center left','center','center right'
+                         'lower left','lower center','lower right']
+    kinds = ['line', 'scatter', 'bar', 'barh', 'boxplot', 'histogram', 'heatmap', 'density']
+    
+    def __init__(self):
+        """Setup variables"""
+  
+        fonts = self.getFonts()
+        opts = self.opts = {'font':{'type':'combobox','default':'Arial','items':fonts},
+                'fontsize':{'type':'scale','default':12,'range':(5,50),'label':'font size'},
+                'marker':{'type':'combobox','default':'','items':self.markers},
+                'linestyle':{'type':'combobox','default':'-','items':self.linestyles},
+                's':{'type':'scale','default':30,'range':(5,500),'label':'marker size'},
+                'grid':{'type':'checkbutton','default':0,'label':'show grid'},
+                #'logx':{'type':'checkbutton','default':0,'label':'log x-axis'},
+                #'logy':{'type':'checkbutton','default':0,'label':'log y-axis'},
+                'legend':{'type':'checkbutton','default':1,'label':'legend'},
+                'kind':{'type':'combobox','default':'line','items':self.kinds,'label':'kind'},
+                'linewidth':{'type':'scale','default':1.5,'range':(0,10),'label':'line width'},
+                'alpha':{'type':'scale','default':0.7,'range':(0,1),'label':'alpha'},
+                'title':{'type':'entry','default':''},
+                'subplots':{'type':'checkbutton','default':0,'label':'multiple subplots'},
+                'colormap':{'type':'combobox','default':'jet','items':self.colormaps}
+                }
+        self.tkvars = {}
+        for i in opts:            
+            t = opts[i]['type']
+            if t in ['entry','combobox']:
+                var = StringVar()
+            elif t in ['checkbutton']:
+                var = IntVar()
+            elif t in ['scale']:
+                var = DoubleVar()
+            var.set(opts[i]['default'])
+            self.tkvars[i] = var
+
+        self.applyOptions()
+        return
+
+    def applyOptions(self):
+        """Set the options"""
+
+        kwds = {}        
+        for i in self.opts:
+            kwds[i] = self.tkvars[i].get()        
+        self.kwds = kwds        
+        plt.rc("font", family=kwds['font'], size=kwds['fontsize'])   
+        return
+
+    def apply(self):
+        self.applyOptions()       
+        if self.callback != None:
+            self.callback()
+        return
+
+    def showDialog(self, parent, callback=None):
+        """Auto create tk vars for options"""
+        
+        opts = self.opts
+        tkvars = self.tkvars
+        dialog = Frame(parent)
+        self.callback = callback
+
+        grps = {'styles':['font','colormap','alpha','grid'],
+                'sizes':['fontsize','s','linewidth'],
+                'format':['kind','marker','linestyle','legend','subplots'],
+                'general':['title']}
+        c=0
+        for g in ['format','sizes','styles','general']:            
+            frame = LabelFrame(dialog, text=g)
+            frame.grid(row=0,column=c,sticky='news')
+            row=0; col=0
+            for i in grps[g]:
+                w=None
+                opt = opts[i]               
+                if opt['type'] == 'entry':             
+                    Label(frame,text=i).pack() 
+                    w = Entry(frame,textvariable=tkvars[i])                               
+                elif opt['type'] == 'checkbutton': 
+                    w = Checkbutton(frame,text=opt['label'],
+                             variable=tkvars[i])       
+                elif opt['type'] == 'combobox':
+                    Label(frame,text=i).pack()           
+                    w = Combobox(frame, values=opt['items'],
+                             textvariable=tkvars[i])                    
+                    w.set(opt['default'])
+                elif opt['type'] == 'scale':
+                    fr,to=opt['range']
+                    w = tkinter.Scale(frame,label=opt['label'],
+                             from_=fr,to=to,
+                             orient='horizontal',
+                             resolution=(to-fr)/30,
+                             variable=tkvars[i])
+                if w != None:                    
+                    w.pack(fill=BOTH,expand=1)
+                row+=1
+            c+=1            
+
+        row=3
+        def close():
+            dialog.destroy()        
+        #frame=Frame(dialog)
+        #frame.grid(row=0,column=c,sticky='nwes')
+        b = Button(frame, text="Apply", command=self.apply)
+        b.pack(side=TOP,fill=X,expand=1)
+        c=Button(frame,text='Close', command=close)
+        c.pack(side=TOP,fill=X,expand=1)
+        return dialog
+
+    def getFonts(self):
+        """Get the current list of system fonts"""
+        fonts = set(list(tkinter.font.families()))
+        fonts = sorted(list(fonts))
+        return fonts
+
+
