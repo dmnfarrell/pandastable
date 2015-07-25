72c06335bc7e1a64ae65436fc6f47ab14c1cb739
dmnfarrell <farrell.damien@gmail.com>
2015-07-25 15:33:02 +0100

    minor

--- freeze.py
+++ freeze.py
@@ -4,13 +4,15 @@ from cx_Freeze import setup, Executable
 sys.path.append('pandastable')
 
 includes = ["pandastable"]
+includefiles = ["pandastable/dataexplore.gif","pandastable/datasets"]
 
 # Dependencies are automatically detected, but it might need fine tuning.
 build_exe_options = {"packages": ["os","numpy","matplotlib","pandas","pandastable"],
                      "excludes": [],
                      "namespace_packages": ['mpl_toolkits'],
                      "include_msvcr": True,
-                     "includes": includes}
+                     "includes": includes,
+                     "include_files": includefiles}
 
 base = None
 if sys.platform == "win32":
