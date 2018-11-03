5ff4c95192ae25fa3a4e8f6f194aa5db141e11c9
dmnfarrell <farrell.damien@gmail.com>
2018-11-03 13:32:40 +0000

    freeze updates

--- freeze.py
+++ freeze.py
@@ -1,32 +1,35 @@
 import sys, os
 from cx_Freeze import setup, Executable
+PYTHON_INSTALL_DIR = os.path.dirname(os.path.dirname(os.__file__))
 
 sys.path.append('pandastable')
 
 #currently requires changing line 548 of hooks.py to make scipy work
 #see https://bitbucket.org/anthony_tuininga/cx_freeze/issues/43
 
-includes = ["pandastable"]#,"scipy.integrate.vode","scipy.integrate.lsoda","scipy.linalg"]
+includes = ["pandastable"]
 includefiles = ["pandastable/dataexplore.gif","pandastable/datasets",
-                "pandastable/plugins"]
+                "pandastable/plugins",
+                os.path.join(PYTHON_INSTALL_DIR, 'DLLs', 'tk86t.dll'),
+                os.path.join(PYTHON_INSTALL_DIR, 'DLLs', 'tcl86t.dll')
+                ]
 
 # Dependencies are automatically detected, but it might need fine tuning.
 build_exe_options = {"packages": ["os","numpy","matplotlib","pandas",
-                                  "scipy","seaborn","IPython",
-                                  "statsmodels","pandastable"],
-                     "excludes": [],
+                                  #"scipy","seaborn","IPython","statsmodels",
+                                  "pandastable"],
+                     "excludes": ['scipy','seaborn','statsmodels'],
                      "namespace_packages": ['mpl_toolkits'],
                      "include_msvcr": True,
                      "includes": includes,
                      "include_files": includefiles}
 
 base = None
-if sys.platform == "win32":
-    #base = None
+if sys.platform == "win32":    
     base = "Win32GUI"
 
 executables = [Executable("main.py", base=base,
-                          copyDependentFiles = True,
+                          #copyDependentFiles = True,
                           targetName='DataExplore.exe',
                           shortcutName="DataExplore",
                           shortcutDir="DesktopFolder",
