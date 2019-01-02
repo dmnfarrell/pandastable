5d3db722a4846991c24b6b5f0211cfa2f0cfeaca
Robert P. Goldman <rpgoldman@goldman-tribe.org>
2019-01-02 17:42:57 -0600

    Run in debug mode
    Add and load debug module that will throw DataExplore into the debugger on exceptions.

---
+++ pandastable/debug.py
@@ -0,0 +1,21 @@
+# When loaded, this python module will automagically start up the
+# debugger in response to exceptions.  Many thanks to tzot on
+# StackExchange: https://stackoverflow.com/a/242531/289934
+
+import sys
+
+def info(type, value, tb):
+    if hasattr(sys, 'ps1') or not sys.stderr.isatty():
+    # we are in interactive mode or we don't have a tty-like
+    # device, so we call the default hook
+        sys.__excepthook__(type, value, tb)
+    else:
+        import traceback, pdb
+        # we are NOT in interactive mode, print the exception...
+        traceback.print_exception(type, value, tb)
+        print
+        # ...then start the debugger in post-mortem mode.
+        # pdb.pm() # deprecated
+        pdb.post_mortem(tb) # more "modern"
+
+sys.excepthook = info
